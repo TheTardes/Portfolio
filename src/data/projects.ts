4c208d5fe8da71402564edb7ee589b80c934cb4c
Inji Mammadli <mammi910@newschool.edu>
2025-07-05 02:46:52 -0400

    removing "work" links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -47,22 +47,22 @@ export const projects: Project[] = [
   //   cardTitle: "Leisure Discovery ✨ addressing trust and exposure challenges ",
   //   tags: ["Visual Design", "UI", "Typography"],
   // },
-  {
-  external: true,
-    url: "https://www.figma.com/deck/vwwPUz2WLbJ2khPYncpuFi/Deloitte-Portolio---Inji-Mammadli?node-id=1-34&t=KdxFPW2D3SHzx39S-1",
-    name: "deloitte-work",
-    cardImage: "cover-deloitte.webp",
-    cardTitle: "UX Strategy, Branding, and Marketing at <span class='text-green-500'>Deloitte</span> & Touche LLC",
-    tags: ["Campaign UX", "Experiential UX", "EX Branding"],
+  // {
+  // external: true,
+  //   url: "https://www.figma.com/deck/vwwPUz2WLbJ2khPYncpuFi/Deloitte-Portolio---Inji-Mammadli?node-id=1-34&t=KdxFPW2D3SHzx39S-1",
+  //   name: "deloitte-work",
+  //   cardImage: "cover-deloitte.webp",
+  //   cardTitle: "UX Strategy, Branding, and Marketing at <span class='text-green-500'>Deloitte</span> & Touche LLC",
+  //   tags: ["Campaign UX", "Experiential UX", "EX Branding"],
 
-  },
-  {
-    external: true,
-      url: "/assets/pdfs/professional-work-portfolio.pdf",
-      name: "work-projects",
-      cardImage: "cover-work.webp",
-      cardTitle: "Check out  highlights from my professional work here 👉🏻",
-      tags: ["Productivity Software", "UX/UI ", "Visual Design"],
-    }
+  // },
+  // {
+  //   external: true,
+  //     url: "/assets/pdfs/professional-work-portfolio.pdf",
+  //     name: "work-projects",
+  //     cardImage: "cover-work.webp",
+  //     cardTitle: "Check out  highlights from my professional work here 👉🏻",
+  //     tags: ["Productivity Software", "UX/UI ", "Visual Design"],
+  //   }
 
 ];
